a89afb07f2448145772f9ae9733eae17c58a5d35
Aditya Gurjar <17155247+adityadroid@users.noreply.github.com>
2023-06-29 17:14:16 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.tipsy_trove
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:tipsy_trove/app/app.dart';
+import 'package:tipsy_trove/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
